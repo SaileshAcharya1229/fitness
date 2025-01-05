483c45386e88e1fee354282d5ac9cfcf2077da38
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2025-01-05 20:39:10 +0545

    addinganother icon

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,45 +8,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+          title: Text(
+            'breakfast',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          alignment:Alignment.center,
-          child: SvgPicture.asset(
-            'assets/icons/arrowleft.svg',
-            height: 20,
-            width: 20,
-          ),
-          decoration: BoxDecoration(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          centerTitle: true,
+          leading: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/icons/arrowleft.svg',
+              height: 20,
+              width: 20,
+            ),
+            decoration: BoxDecoration(
               color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10),),
-        ),
-        actions:[
-            Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              child: SvgPicture.asset(
-                'assets/icons/arrowleft.svg',
-                height: 20,
-                width: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          actions: [
+            GestureDetector(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                width: 37,
+                child: SvgPicture.asset(
+                  'assets/icons/dots.svg',
+                  height: 5,
+                  width: 5,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffF7F8F8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
-          ]
-      ),
+          ]),
     );
   }
 }
